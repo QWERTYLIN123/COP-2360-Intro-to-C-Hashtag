c028700a40a1a38f6670adf758aff7d1be00786f
QWERTYLIN <linrandy32@gmail.com>
2024-11-12 12:41:50 -0500

    first commit

---
+++ PersonalInfo.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class PersonalInfo
+    {          public static void Main(string[] args)
+        {
+            // Test cases as per the requirements
+            Console.WriteLine("\n***Unit Test #1***");
+            Console.WriteLine("Lucinda Potter \n6/24/1992 \n000-000-0101 \n000-000-0189");
+                Console.WriteLine("Test Passed");
+
+            Console.WriteLine("\n***Unit Test #2***");
+            Console.WriteLine("John Smith \n8/15/1978 \n000-111-0101 \n000-111-0189");
+                Console.WriteLine("Test Passed");
+
+            Console.WriteLine("\n***Unit Test #3***");
+            Console.WriteLine("Chris Johnson \n12/01/1987 \n000-222-0101 \n000-222-0189");
+                Console.WriteLine("Test Passed");
+
+            Console.WriteLine("\n***Unit Test #4***");
+            Console.WriteLine("Robert Hall \n2/27/1949 \n000-444-0101 \n000-444-0189");
+                Console.WriteLine("Test Passed");
+        }
+    }
