c7b172074b66d40ef590951c01beb06dc1c04e81
QWERTYLIN <linrandy32@gmail.com>
2024-11-12 19:09:35 -0500

    Committing for Lyrics.cs

---
+++ Lyrics/Lyrics.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Lyrics
+    {
+        public static void Main(string[] args)
+        {
+            //Unit Test 1 Lyrics
+            Console.WriteLine("\nUnit Test 1");
+            Console.WriteLine("Oh, well imagine / ");
+            Console.WriteLine("As I'm pacing the pews in a church corridor /");
+            Console.WriteLine("And I can't help but to hear /");
+            Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+            Console.WriteLine("   *Test Passed*");
+
+            //Unit Test 2 Lyrics
+            Console.WriteLine("\nUnit Test #2");
+            Console.WriteLine("Oh-oh-oh-oh-oh, \noh-oh-oh-oh, \noh-oh-oh / \nCaught in a bad romance...");
+            Console.WriteLine("   *Test Passed*");
+
+            //Unit Test 3 Lyrics
+            Console.WriteLine("\nUnit Test 3");
+            Console.WriteLine("Coming out of my cage \nAnd I've been doing just fine \nGotta gotta be down \nBecause I want it all");
+            Console.WriteLine("   *Test Passed*");
+
+            //Unit Test 4 Lyrics
+            Console.WriteLine("\nUnit Test 4");
+            Console.WriteLine("Now, this is a story all about how \nMy life got flipped-turned upside down \nAnd I'd like to take a minute \nJust sit right there \nI'll tell you how I became the prince of a town called Bel-Air");
+            Console.WriteLine("   *Test Passed*");
+        }
+
+    }
+
